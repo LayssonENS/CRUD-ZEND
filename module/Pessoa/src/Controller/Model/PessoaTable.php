7dbddebbe916748461762a903e9caad988619aa0
Laysson Enderson <laysson_enderson@hotmail.com>
2019-03-16 10:31:04 -0300

    module

--- module/Pessoa/src/Controller/Model/PessoaTable.php
+++ module/Pessoa/src/Controller/Model/PessoaTable.php
@@ -24,15 +24,35 @@ class PessoaTable
 
     public function getPessoa($id)
     {
-        $id = (int) $id;
+        $id = (int)$id;
 
         $rowset = $this->tableGateway->select(['id' => $id]);
         $row = $rowset->current();
         if (!$row) {
             throw new  RuntimeException(sprintf('Não foi encontrado o id %d', $id));
-
         }
         return $row;
     }
 
+    public function salvarPessoa(Pessoa $pessoa)
+    {
+        $data = [
+            'nome' => $pessoa->getNome(),
+            'sobrenome' => $pessoa->getSobrenome(),
+            'email' => $pessoa->getEmail(),
+            'situacao' => $pessoa->getSituacao(),
+        ];
+
+        $id = (int)$pessoa->getId();
+        if ($id === 0) {
+            $this->tableGateway->insert($data);
+            return;
+        }
+        $this->tableGateway->update($data, ['id' => $id]);
+    }
+
+    public function deletarPessoa($id)
+    {
+        $this->tableGateway->delete(['id' => (int)$id]);
+    }
 }
--- module/Pessoa/src/Module.php
+++ module/Pessoa/src/Module.php
@@ -3,10 +3,33 @@
 namespace Pessoa;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Pessoa\Model\PessoaTable;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\Feature\EventFeature\TableGatewayEvent;
 
 class Module implements ConfigProviderInterface {
 
-    public function getConfig(){
+    public function getConfig()
+    {
         return include __DIR__."/../config/module.config.php";
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\PessoaTable::class => function($container){
+                    $tableGateway = $container->get(Model\PessoaTableGateway::class);
+                    return new PessoaTable($tableGateway);
+                },
+                Model\PessoaTableGateway::class => function($container){
+                    $dbAdapter = $container->(AdapterInterface::class);
+                    $resultSerPrototype = new ResultSet();
+                    $resultSerPrototype->setArrayObjectPrototype(new Model\Pessoa());
+                    return new TableGateway('pessoa', $dbAdapter,null,$resultSerPrototype);
+                },
+            ]
+        ];
     }
 }
