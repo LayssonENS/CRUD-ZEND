1ab29c8011556fa72fef286be96d6694c1925e68
Laysson Enderson <laysson_enderson@hotmail.com>
2019-03-22 11:41:45 -0300

    teste

--- module/Pessoa/src/Form/PessoaForm.php
+++ module/Pessoa/src/Form/PessoaForm.php
@@ -3,6 +3,7 @@ namespace Pessoa\Form;
 
 use Zend\Form\Form;
 use Zend\Form\Element;
+use Zend\Captcha;
 
 
 /**
@@ -10,26 +11,24 @@ use Zend\Form\Element;
  *
  * @author drink
  */
-class PessoaForm extends Form {
- 
-    public function __construct() {
+class PessoaForm extends Form
+{
+
+    public function __construct()
+    {
         parent::__construct('pessoa', []);
-        
+
         $this->add(new \Zend\Form\Element\Hidden('id'));
-        $this->add(new \Zend\Form\Element\Text("nome",['label' => "Nome"]));
-        $this->add(new \Zend\Form\Element\Text("sobrenome",['label' => "Sobrenome"]));
-        $this->add(new \Zend\Form\Element\Email("email",['label' => "email"]));
+        $this->add(new \Zend\Form\Element\Text("nome", ['label' => "Nome"]));
+        $this->add(new \Zend\Form\Element\Text("sobrenome", ['label' => "Sobrenome"]));
+        $this->add(new \Zend\Form\Element\Email("email", ['label' => "email"]));
         $this->add(new \Zend\Form\Element\Select('situacao', ['label' => "Situação", 'value_options' => [
             'Ativo' => 'Ativo',
             'Inativo' => 'Inativo',
         ]]));
 
-       
         $submit = new \Zend\Form\Element\Submit('submit');
-        $submit->setAttributes(['value'=>'Salvar','id'=>'submitbutton']);
+        $submit->setAttributes(['value' => 'Salvar', 'id' => 'submitbutton']);
         $this->add($submit);
-
-        
     }
-    
 }
